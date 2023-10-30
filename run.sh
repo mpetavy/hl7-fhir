ea3cec66969d7bcee3977fe323668660191eb396
mpetavy <petavy@gmx.net>
2023-10-30 20:21:43 +0100

    run.sh added

---
+++ run.sh
@@ -0,0 +1,3 @@
+curl -v -k -X POST -H "Content-Type: application/json; charset=utf-8" --data @patient.json https://hapi.fhir.org/baseR4/Patient
+
+curl -v -k -H "Content-Type: application/json; charset=utf-8" https://hapi.fhir.org/baseR4/Patient/32819462
